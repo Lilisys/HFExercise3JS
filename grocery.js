d7ab8c040f5c0e9bf3536a666487630be4c42d90
Lilisys <jesswu@umich.edu>
2016-06-20 09:57:18 -0400

    init and done

---
+++ grocery.js
@@ -0,0 +1,41 @@
+var groceryItems =[
+	{ name: 'pineapple',	price: '3.00'},
+	{ name: 'coconut',		price: '4.00'},
+	{ name: 'tequila',		price: '8.00'},
+	{ name: 'lime',				price: '1.43'}
+];
+
+function updateList(){
+	//Clear out the text on page
+	var p = document.getElementById('grocerp');
+	p.innerHTML = "";
+	
+	//Update the total
+	var total = 0;
+	for (var i = 0; i < groceryItems.length; i++) {
+	 	
+	 	p.innerHTML += "Name: " + groceryItems[i].name + ", Price: " + groceryItems[i].price + '<br>'; 
+	 	console.log("Name:", groceryItems[i].name, "Price: ", groceryItems[i].price);
+	 	total += parseFloat(groceryItems[i].price);
+	 }
+
+	//Update the page
+	p.innerHTML += "Total: " + total;
+	console.log("Total: ", total);
+}
+
+function addItem(){
+	var nameIn = document.getElementById("itemName");
+	var priceIn = document.getElementById("itemPrice");
+	var newName = nameIn.value;
+	var newPrice = priceIn.value;
+	groceryItems.push({name: newName, price: newPrice});
+	updateList();
+
+	nameIn.value = "";
+	priceIn.value = "";
+	nameIn.placeholder = "Item Name";
+	nameIn.placeholder = "Item Price";
+}
+
+updateList();
